4d3b379e093b9cd44564e23abb469003ee20f3ff
rawalnishant-769 <rawalnishant769@gmail.com>
2026-05-12 08:38:41 +0530

    normal commit

---
+++ day6/crazy/if_statement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "elgible for vote." << endl;
+    } 
+      return 0;
+}
+

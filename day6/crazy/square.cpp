14db8d09212d513080eb3f6ca837cec5376f0330
rawalnishant-769 <rawalnishant769@gmail.com>
2026-05-12 08:29:59 +0530

    normal commit

---
+++ day6/crazy/square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main() {
+    int num, square;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    square = num * num;
+
+    cout << "Square = " << square << endl;
+
+    return 0;
+}
